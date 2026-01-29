c2fe4edc8bedc862bfa4d82dbf90e6e54f2f9b92
xsaz0826 <xsaz0826@gmail.com>
2026-01-29 11:37:17 +0900

    [수정] 게시판 수정

--- springJDBCBoard/src/main/java/com/board/service/BoardServiceImpl.java
+++ springJDBCBoard/src/main/java/com/board/service/BoardServiceImpl.java
@@ -1,9 +1,11 @@
 package com.board.service;
 
+import java.beans.Transient;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.board.dao.BoardDAO;
 import com.board.domain.Board;
@@ -14,6 +16,7 @@ public class BoardServiceImpl implements BoardService {
 	private BoardDAO boardDAO;
 	
 	@Override
+	@Transactional
 	public int insertBoard(Board board) throws Exception {
 		int count = boardDAO.insertBoard(board);
 		return count;
@@ -26,12 +29,14 @@ public class BoardServiceImpl implements BoardService {
 	}
 
 	@Override
+	@Transactional
 	public int updateBoard(Board board) throws Exception {
 		int count = boardDAO.updateBoard(board);
 		return count;
 	}
 
 	@Override
+	@Transactional
 	public int deleteBoard(Board board) throws Exception {
 		int count = boardDAO.deleteBoard(board);
 		return count;
